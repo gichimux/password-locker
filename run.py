360cc155a5d1b1f72ab8e9a749bba93ddb35674b
gichimux <gichimumwai1@gmail.com>
2019-04-11 10:53:49 +0300

    removed code that was generating bugs

--- run.py
+++ run.py
@@ -74,7 +74,7 @@ def main():
             my_id+=1
             print(f" {my_username} has been initiated .\nLogin to continue")
             entries.append(0)
-            print("*"*20)
+            print("-"*20)
        
         #log in interface
         elif input_text == "ln".lower():
@@ -86,7 +86,7 @@ def main():
             if get_result == 0:
                 print("\n")
                 print("Invalid username and/or password")
-                print("*"*25)
+                print("-"*25)
             elif get_result!=0:
                 
                 #logged in user's interface
@@ -121,11 +121,8 @@ def main():
                         time.sleep(1.5)
                         print("\n")
                         print(f"***Your password for {my_app} is {my_webkey}***")
-<<<<<<< HEAD
-                        print("*"*45)
-=======
-                        print("+"*30)
->>>>>>> 681834cb4657d7fa47d8232838b23a2c7e27ba96
+                        print("-"*45)
+                        print("-"*30)
                     #interface to view passwords
                     elif get_input == "vp":
                         if user_existing(get_result.auth):
@@ -138,17 +135,10 @@ def main():
                                 print(f"{my_label+1}. {get_password.app_name} >>> {get_password.web_key}")
                                 my_label+=1
                             print("\nEnter a command to continue")
-<<<<<<< HEAD
-                            print("*"*20)
+                            print("-"*20)
                         else:
                             print("\nYou have no saved passwords.\nType ap to generate some passwords")
-                            print("*"*20)
-=======
-                            print("+"*20)
-                        else:
-                            print("\nYou have no saved passwords.\nType ap to generate some passwords")
-                            print("+"*20)
->>>>>>> 681834cb4657d7fa47d8232838b23a2c7e27ba96
+                            print("-"*20)
 
                     elif get_input == "cp": #copy password interface
                         if user_existing(get_result.auth):
@@ -158,24 +148,20 @@ def main():
                                 print("\n")
                                 print(f"{get_index+1} is invalid. Enter the correct index of password to copy")
                                 print("Type vp to confirm the correct index of password to copy")
-                                print("+"*20)
+                                print("-"*20)
                             elif get_index < entries[get_result.auth]:
                                 copy_password(get_result.auth,get_index)
                                 print("\n")
                                 print(f"Password {get_index+1} on the list has been copied, and is ready for pasting")
-                                print("+"*30)
+                                print("-"*30)
                         else:
                             print("\nYou have no saved passwords.\nType ap to add some passwords")
-                            print("*"*20)
+                            print("-"*20)
 
                     elif get_input == "lo":
                         print("\n")
                         print(f" {get_result.user_name} has been logged out")
-<<<<<<< HEAD
-                        print("*"*30)
-=======
-                        print("+"*30)
->>>>>>> 681834cb4657d7fa47d8232838b23a2c7e27ba96
+                        print("-"*30)
                         break
                     
                     else:
@@ -185,16 +171,12 @@ def main():
         elif input_text == "ex":
             print("\n")
             print(f"You have exited Password Locker!")
-            print("+"*30)
+            print("="*30)
             break
 
         else:
             print("Invalid entry. Enter command again")
-<<<<<<< HEAD
-            print("\n"+"*"*20)
-=======
-            print("\n"+"*"*40)
->>>>>>> 681834cb4657d7fa47d8232838b23a2c7e27ba96
+            print("\n"+"="*20)
 
 
 if __name__ == '__main__':
